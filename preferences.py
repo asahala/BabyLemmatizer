b4e1fc24b3f86defeae141e4cef57d93d8a15d3e
asahala <aleksi.sahala@helsinki.fi>
2023-02-03 01:48:46 +0200

    update

--- preferences.py
+++ preferences.py
@@ -1,5 +1,11 @@
+import os
+
 """ Virtual environment path that contains all requirements for OpenNMT """
 python_path = '/projappl/clarin/onmt/OpenNMT/bin/'
 
 """ OpenNMT-Py path, i.e. where the OpenNMT binaries are """
 onmt_path = './OpenNMT-py/onmt/bin/'
+
+if __name__ == "__main__":
+    os.system(f'{python_path}python {onmt_path}train.py -h')
+
